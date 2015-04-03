5af20b72c74468c2990a2e507f835a83ed177aea
Prasanna Rodrigo <rodrigo.rajitha@gmail.com>
2015-04-03 21:21:04 +0530

    add events to tabs

--- js/transform.js
+++ js/transform.js
@@ -1,5 +1,6 @@
 $(function() {
-    var open = false;
+    
+    var open = false, about = false, register = false, news = false, contact = false;
     $( '#sb-container' ).swatchbook( {
         // number of degrees that is between each item
         angleInc : 15,
@@ -11,28 +12,137 @@ $(function() {
         closeIdx : 11
     } );
     
+    display_none();
+    
+    // about handler
     $( '.dv-about' ).click(function(){
         var left_dist = "+=650";
-        if(open){
+        if(open && !about){
+            display_none();
+            $( '.text-content-about' ).css('display','block');
+            $( '.text-content-about' ).delay(1300).animate({"opacity": '1'}, "slow");
+            about = true; register = false; news = false; contact = false;
         }
-        else{
+        else if(!open){
+            // hide text-content span
             $( '.text-content' ).css('opacity', '0');
+            // expand the div
             $( '.content' ).delay(700).animate({"width":left_dist}, "slow");
-            $( '.text-content' ).delay(1300).animate({"opacity": '1'}, "slow");
-            $( '#sb-container' ).delay(1000).animate({"opacity": '0.5'}, "slow");
+            // appear about span
+            $( '.text-content-about' ).css('display','block');
+            // show about content text
+            $( '.text-content-about' ).delay(1300).animate({"opacity": '1'}, "slow");
+            
+            // change opacity of swatchbook
+            swatchbook_opac();
+            // book opened
             open = true;
+            // selected tab
+            about = true;
         }
         
     });
     
-    $('.dv-home').click(function(){
+    // register handler
+    $( '.dv-register' ).click(function(){
+        var left_dist = "+=650";
+        if(open && !register){
+            display_none();
+            $( '.text-content-register' ).css('display','block');
+            $( '.text-content-register' ).delay(1300).animate({"opacity": '1'}, "slow");
+            register = true; about = false; news = false; contact = false;
+        }
+        else if(!open){
+            $( '.text-content' ).css('opacity', '0');
+            $( '.content' ).delay(700).animate({"width":left_dist}, "slow");
+            $( '.text-content-register' ).css('display','block');
+            $( '.text-content-register' ).delay(1300).animate({"opacity": '1'}, "slow");
+            
+            swatchbook_opac();
+            open = true;
+            register = true;
+        }
+        
+    });
+    
+    // news handler
+    $( '.dv-news' ).click(function(){
+        var left_dist = "+=650";
+        if(open && !register){
+            display_none();
+            $( '.text-content-news' ).css('display','block');
+            $( '.text-content-news' ).delay(1300).animate({"opacity": '1'}, "slow");
+            news = true; about = false; register = false; contact = false;
+        }
+        else if(!open){
+            $( '.text-content' ).css('opacity', '0');
+            $( '.content' ).delay(700).animate({"width":left_dist}, "slow");
+            $( '.text-content-news' ).css('display','block');
+            $( '.text-content-news' ).delay(1300).animate({"opacity": '1'}, "slow");
+            
+            swatchbook_opac();
+            open = true;
+            news = true;
+        }
+        
+    });
+    
+    // contact handler
+    $( '.dv-contact' ).click(function(){
+        var left_dist = "+=650";
+        if(open && !register){
+            display_none();
+            $( '.text-content-contact' ).css('display','block');
+            $( '.text-content-contact' ).delay(1300).animate({"opacity": '1'}, "slow");
+            news = false; about = false; register = false; contact = true;
+        }
+        else if(!open){
+            $( '.text-content' ).css('opacity', '0');
+            $( '.content' ).delay(700).animate({"width":left_dist}, "slow");
+            $( '.text-content-contact' ).css('display','block');
+            $( '.text-content-contact' ).delay(1300).animate({"opacity": '1'}, "slow");
+            
+            swatchbook_opac();
+            open = true;
+            contact = true;
+        }
+        
+    });
+    
+    // home handler
+    $( '.dv-home' ).click(function(){
         var left_dist = "-=650";
         if(open){
             $( '.content' ).delay(700).animate({"width":left_dist}, "slow");
             $( '.text-content' ).fadeTo("slow","0.0");
-            $( '#sb-container' ).delay(1000).animate({"opacity": '1'}, "slow");
+            display_none();
+            $( '#sb-container > .dv-nav').each(function () { 
+                $(this).delay(100).animate({"opacity": '1'}, "slow");
+            });
+            // book closed
             open = false;
         }
     });
+
+    $(".dv-nav-h").hover(
+        function() {
+            if(open) $(this).animate({opacity: 8.0}, 100);
+        }, function() {
+            if(open) $(this).animate({opacity: 0.3}, 100);
+        }
+    );
     
 });
+
+function display_none(){
+        $( '.text-content-register' ).css('display','none');
+        $( '.text-content-about' ).css('display','none');
+        $( '.text-content-news' ).css('display','none');
+        $( '.text-content-contact' ).css('display','none');
+}
+
+function swatchbook_opac(){
+    $( '#sb-container > .dv-nav').each(function () { 
+        $(this).delay(100).animate({"opacity": '0.3'}, "slow");
+    });
+}
